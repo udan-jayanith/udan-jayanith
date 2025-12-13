e5dd74ac60d9641aaa4f2181a2e5272eb9869780
Udan Jayanith <j.udanjayanith@gmail.com>
2025-12-14 01:40:48 +0530

    Changes

--- src/components/ExperienceSection/ExperienceSection.vue
+++ src/components/ExperienceSection/ExperienceSection.vue
@@ -8,14 +8,13 @@
 			<a href="https://github.com/udan-jayanith/HLS">HTTP Live streaming</a> and
 			<a href="https://github.com/udan-jayanith/Quick">Quic</a>.
 		</li>
-		<li>
-			Developed parsers (HTTP and HLS), serializers (HTML, Quic, HTTP and HLS) and complex-algorithms
-			(querying) by my slef.
-		</li>
 		<li>Preficent in Go and Javascript and worked with Vue (For this site and other web apps).</li>
 		<li>
 			Worked with various databases (Bbolt, SQLite, MySQL, Firebase Firestore and Badger) and have
 			studied database internals.
+		</li>
+		<li>
+			Developed parsers (HTTP and HLS) and serializers (HTML, Quic, HTTP and HLS).
 		</li>
 		<li>
 			Has developed Chrome extensions (Download Manager and Chrome new tab page) and various applications
