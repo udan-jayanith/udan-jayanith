d77225909797835cc97084e72f09ee9d43e67f81
Udan Jayanith <j.udanjayanith@gmail.com>
2025-12-15 19:58:03 +0530

    fixed typos

--- app/components/ExperienceSection/ExperienceSection.server.vue
+++ app/components/ExperienceSection/ExperienceSection.server.vue
@@ -3,12 +3,12 @@
 <template>
 	<ul>
 		<li>
-			I have implemented various newtwork protocols from scratch including
+			I have implemented various network protocols from scratch including
 			<a href="https://github.com/udan-jayanith/HTTP-V1.1" target="_blank">HTTP</a>,
 			<a href="https://github.com/udan-jayanith/HLS" target="_blank">HTTP Live streaming</a> and
 			<a href="https://github.com/udan-jayanith/Quick" target="_blank">Quic</a>.
 		</li>
-		<li>Preficent in Go and Javascript and worked with Vue (For this site and other web apps).</li>
+		<li>Proficient in Go and Javascript and worked with Vue (For this site and other web apps).</li>
 		<li>
 			Worked with various databases (Bbolt, SQLite, MySQL, Firebase Firestore and Badger) and have
 			studied database internals.
