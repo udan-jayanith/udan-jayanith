f88f99b0b8464d6d930dc9258a60dc04a53c02cd
Udan Jayanith <j.udanjayanith@gmail.com>
2025-12-19 15:01:08 +0530

    Fixed H1UserAgentFontSizeInSection and improved layout shift

--- app/components/ExperienceSection/ExperienceSection.server.vue
+++ app/components/ExperienceSection/ExperienceSection.server.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-	<ul>
+	<ul class="experience-section">
 		<li>
 			I have implemented various network protocols from scratch including
 			<a href="https://github.com/udan-jayanith/HTTP-V1.1" target="_blank">HTTP</a>,
@@ -13,14 +13,16 @@
 			Worked with various databases (Bbolt, SQLite, MySQL, Firebase Firestore and Badger) and have
 			studied database internals.
 		</li>
+		<li>Developed parsers (HTTP and HLS) and serializers (HTML, Quic, HTTP and HLS).</li>
 		<li>
-			Developed parsers (HTTP and HLS) and serializers (HTML, Quic, HTTP and HLS).
-		</li>
-		<li>
-			Has developed Chrome extensions (Download Manager and Chrome new tab page) and various applications
-			(Student management system and Movies/Tv shows discovery+bookmarker)
+			Has developed Chrome extensions (Download Manager and Chrome new tab page) and various
+			applications (Student management system and Movies/Tv shows discovery+bookmarker)
 		</li>
 	</ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+.experience-section {
+	min-height: 180px;
+}
+</style>
